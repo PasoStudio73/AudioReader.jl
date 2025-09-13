a43d17a1d948eb79c787386ea7965ce16a435cc8
PasoStudio73 <paso.studio73@gmail.com>
2025-09-13 21:41:02 +0200

    fix python

--- docs/make.jl
+++ docs/make.jl
@@ -1,29 +1,28 @@
 using AudioReader
 using Documenter
 
-# DocMeta.setdocmeta!(AudioReader, :DocTestSetup, :(using AudioReader); recursive = true)
+DocMeta.setdocmeta!(AudioReader, :DocTestSetup, :(using AudioReader); recursive = true)
 
 makedocs(;
     modules = [AudioReader],
-    # doctest = true,
-    # linkcheck = true,
     authors = "Riccardo Pasini",
-    # repo=Documenter.Remotes.GitHub("aclai-lab", "AudioReader.jl"),
+    repo=Documenter.Remotes.GitHub("PasoStudio73", "AudioReader.jl"),
     sitename = "AudioReader.jl",
-    # format = Documenter.HTML(;
-    #     prettyurls = get(ENV, "CI", "false") == "true",
-    #     canonical = "https://aclai-lab.github.io/AudioReader.jl",
-    # ),
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://PasoStudio73.github.io/AudioReader.jl",
+    ),
     pages = [
-        "Home" => "index.md",
+        "Home"      => "index.md",
+        "Reference" => "reference.md",
     ],
     warnonly = :true,
 )
 
-# deploydocs(;
-#     repo = "github.com/aclai-lab/ModalAssociationRules.jl",
-#     devbranch = "main",
-#     target = "build",
-#     branch = "gh-pages",
-#     versions = ["main" => "main", "stable" => "v^", "v#.#"],
-# )
+deploydocs(;
+    repo = "github.com/PasoStudio73/AudioReader.jl",
+    devbranch = "main",
+    target = "build",
+    branch = "gh-pages",
+    versions = ["main" => "main", "stable" => "v^", "v#.#"],
+)
