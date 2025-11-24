4412b79bfb05e2d044d94f1748852a2435c88a0c
PasoStudio73 <paso.studio73@gmail.com>
2025-11-24 16:26:40 +0100

    tested against matlab

--- test/audiofile.jl
+++ test/audiofile.jl
@@ -59,17 +59,24 @@ end
 matlab_files_dir()    = joinpath(dirname(@__FILE__), "matlab_files")
 matlab_file(filename) = joinpath(matlab_files_dir(), filename)
 
+@testset "against matlab" begin
     matfile_wav = matlab_file("matlab_audioread_wav.mat")
     matfile_mp3 = matlab_file("matlab_audioread_mp3.mat")
+    resampled_wav = matlab_file("matlab_resampled_wav.mat")
 
-mat_wav = MAT.matread(matfile_wav)
-wav_data_matlab = mat_wav["audio_wav"]
+    mat_wav = MAT.matread(matfile_wav)
+    wav_data_mat = mat_wav["audio_wav"]
 
-mat_mp3 = MAT.matread(matfile_mp3)
-mp3_data_matlab = mat_mp3["audio_mp3"]
+    a911_wav = AudioReader.load(wav_file, format=Float64)
+    wav_data_a911 = AudioReader.get_data(a911_wav)
 
-    a911_wav = AudioReader.load(wav_file)
-    wav_data_a911 = AudioReader.data(a911_wav)
+    @test isapprox(wav_data_a911, wav_data_mat)
 
-    a911_mp3 = AudioReader.load(mp3_file)
-    mp3_data_a911 = AudioReader.data(a911_mp3)
+    mat_mp3 = MAT.matread(matfile_mp3)
+    mp3_data_mat = mat_mp3["audio_mp3"]
+
+    a911_mp3 = AudioReader.load(mp3_file, mono=false)
+    mp3_data_a911 = AudioReader.get_data(a911_mp3)
+
+    @test isapprox(mp3_data_mat, mp3_data_a911)
+end
